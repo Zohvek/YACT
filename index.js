5a7afeb35e276ad51ee1e05dc1d0c1a4c7646774
Ryan Hutton <ryanscotthutton@gmail.com>
2021-01-01 19:16:22 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const colors = require('colors');
+const pad = require('pad');
+const needle = require('needle');
+const moment = require('moment');
+const _ = require('lodash');
+const os = require('os');
+const fs = require('fs');
+
+/* 
+Get API data, display API data.. seems easy enough.
+
+
+*/
